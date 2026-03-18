8499df602e47c730d615e5b184269065342f54b1
Mehmet Oktay Ozsoy <mozsoy@192.168.1.52>
2026-03-18 05:58:45 +0300

    example size is bigger now

--- rust_menu/src/main.rs
+++ rust_menu/src/main.rs
@@ -10,14 +10,19 @@ use std::io::{self, Write};
 fn main() -> io::Result<()> {
     let mut table = Table::new(40, 6);
     table.add_data(vec![
-        vec!["12232322", "43", "74", "2", "3", "6"],
-        vec!["23", "54", "85", "32", "4", "7"],
-        vec!["34", "65", "96", "43", "5", "8"],
-        vec!["45", "76", "107", "54", "6", "9"],
-        vec!["56", "87", "118", "54", "6", "10"],
+        vec!["A1", "B1", "C1", "D1", "E1", "F1", "G1", "H1", "I1", "J1"],
+        vec!["A2", "B2", "C2", "D2", "E2", "F2", "G2", "H2", "I2", "J2"],
+        vec!["A3", "B3", "C3", "D3", "E3", "F3", "G3", "H3", "I3", "J3"],
+        vec!["A4", "B4", "C4", "D4", "E4", "F4", "G4", "H4", "I4", "J4"],
+        vec!["A5", "B5", "C5", "D5", "E5", "F5", "G5", "H5", "I5", "J5"],
+        vec!["A6", "B6", "C6", "D6", "E6", "F6", "G6", "H6", "I6", "J6"],
+        vec!["A7", "B7", "C7", "D7", "E7", "F7", "G7", "H7", "I7", "J7"],
+        vec!["A8", "B8", "C8", "D8", "E8", "F8", "G8", "H8", "I8", "J8"],
+        vec!["A9", "B9", "C9", "D9", "E9", "F9", "G9", "H9", "I9", "J9"],
+        vec!["A10", "B10", "C10", "D10", "E10", "F10", "G10", "H10", "I10", "J10"],
     ]);
-    table.add_header_rows(vec!["Col A", "Col B", "Col C", "Col D", "Col E", "Col F"]);
-    table.add_header_cols(vec!["Row 1", "Row 2", "Row 3", "Row 4", "Row 5"]);
+    table.add_header_rows(vec!["Col A", "Col B", "Col C", "Col D", "Col E", "Col F", "Col G", "Col H", "Col I", "Col J"]);
+    table.add_header_cols(vec!["R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "R10"]);
     table.compile();
 
     let mut r = 0;
